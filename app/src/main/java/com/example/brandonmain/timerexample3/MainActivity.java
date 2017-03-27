130263416ab2e0e259e30717250e10ee82d828b0
Salyder <bml998@live.com>
2017-03-27 16:14:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brandonmain/timerexample3/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.brandonmain.timerexample3;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mStartButton;
+    private Button mPauseButton;
+    private Button mResetButton;
+    private Chronometer mChronometer;
+
+    private long lastPause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mPauseButton = (Button) findViewById(R.id.pause_button);
+        mResetButton = (Button) findViewById(R.id.reset_button);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lastPause != 0){
+                    mChronometer.setBase(mChronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+                }
+                else{
+                    mChronometer.setBase(SystemClock.elapsedRealtime());
+                }
+
+                mChronometer.start();
+                mStartButton.setEnabled(false);
+                mPauseButton.setEnabled(true);
+            }
+        });
+
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lastPause = SystemClock.elapsedRealtime();
+                mChronometer.stop();
+                mPauseButton.setEnabled(false);
+                mStartButton.setEnabled(true);
+            }
+        });
+
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mChronometer.stop();
+                mChronometer.setBase(SystemClock.elapsedRealtime());
+                lastPause = 0;
+                mStartButton.setEnabled(true);
+                mPauseButton.setEnabled(false);
+            }
+        });
+    }
+}
